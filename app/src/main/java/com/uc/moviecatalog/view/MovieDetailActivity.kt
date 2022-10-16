380359bfc7b62a033a323deabce6b57016e682ef
kennethraff <kraffelino@student.ciputra.ac.id>
2022-10-16 11:59:38 +0700

    Final loading aman

--- app/src/main/java/com/uc/moviecatalog/view/MovieDetailActivity.kt
+++ app/src/main/java/com/uc/moviecatalog/view/MovieDetailActivity.kt
@@ -2,6 +2,7 @@ package com.uc.moviecatalog.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.HorizontalScrollView
 import android.widget.Toast
 import androidx.lifecycle.Observer
@@ -40,6 +41,18 @@ class MovieDetailActivity : AppCompatActivity() {
         binding= ActivityMovieDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tvTitleMovieDetail.visibility = View.INVISIBLE
+        binding.textviewOverviewMovieDetail.visibility = View.INVISIBLE
+        binding.imagePosterMovieDetail.visibility = View.INVISIBLE
+        binding.recyclerViewGenreMovieDetail.visibility = View.INVISIBLE
+        binding.recyclerViewCompanyMovieDetail.visibility = View.INVISIBLE
+        binding.recyclerViewCountryMovieDetail.visibility = View.INVISIBLE
+        binding.textView.visibility = View.INVISIBLE
+        binding.textView2.visibility = View.INVISIBLE
+        binding.textView3.visibility = View.INVISIBLE
+        binding.textView4.visibility = View.INVISIBLE
+
+
         buatKeHorizontalScrollView = LinearLayoutManager(this)
         buatKeHorizontalScrollView.orientation= LinearLayoutManager.HORIZONTAL
 
@@ -57,9 +70,23 @@ class MovieDetailActivity : AppCompatActivity() {
 
 
         viewModel.movieDetails.observe(this, Observer { response->
+
+            if (response != null){
+                binding.progressBarMovieDetail.visibility= View.INVISIBLE
+                binding.tvTitleMovieDetail.visibility = View.VISIBLE
+                binding.textviewOverviewMovieDetail.visibility = View.VISIBLE
+                binding.imagePosterMovieDetail.visibility = View.VISIBLE
+                binding.textView.visibility = View.VISIBLE
+                binding.textView2.visibility = View.VISIBLE
+                binding.textView3.visibility = View.VISIBLE
+                binding.textView4.visibility = View.VISIBLE
+            }
+
             binding.tvTitleMovieDetail.apply {
                 text= response.title
         }
+
+
             binding.textviewOverviewMovieDetail.apply {
                 text= response.overview
             }
@@ -67,19 +94,35 @@ class MovieDetailActivity : AppCompatActivity() {
                 .load(Const.IMG_URL+ response.backdrop_path)
                 .into(binding.imagePosterMovieDetail)
         })
+
         viewModel.movieGenre.observe(this, Observer { response->
+
+            if (response != null){
+                binding.recyclerViewGenreMovieDetail.visibility = View.VISIBLE
+            }
+
             binding.recyclerViewGenreMovieDetail.layoutManager= buatKeHorizontalScrollView
             moviegenreAdapter= GenreAdapter(response as ArrayList<Genre>)
             binding.recyclerViewGenreMovieDetail.adapter= moviegenreAdapter
         })
 
         viewModel.movieCompany.observe(this, Observer { response->
+
+            if (response != null){
+                binding.recyclerViewCompanyMovieDetail.visibility = View.VISIBLE
+            }
+
             binding.recyclerViewCompanyMovieDetail.layoutManager= buatKeHorizontalScrollViewCompany
             movieCompanyAdapter= CompanyAdapter(response as ArrayList<ProductionCompany>)
             binding.recyclerViewCompanyMovieDetail.adapter= movieCompanyAdapter
         })
 
         viewModel.movieCountry.observe(this, Observer { response->
+
+            if (response != null){
+                binding.recyclerViewCountryMovieDetail.visibility = View.VISIBLE
+            }
+
             binding.recyclerViewCountryMovieDetail.layoutManager= buatKeHorizontalScrollViewCountry
             movieCountryAdapter= CountryAdapter(response as ArrayList<ProductionCountry>)
             binding.recyclerViewCountryMovieDetail.adapter= movieCountryAdapter
